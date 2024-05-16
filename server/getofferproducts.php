c0c945f2ed7d92a2a52fa3f09b643d3edd43f36d
kay mudau <kkay.mudau008@gmail.com>
2024-05-17 01:05:09 +0200

    committed

--- server/getofferproducts.php
+++ server/getofferproducts.php
@@ -1,12 +1,6 @@
 <?php
 include('connection.php');
-<<<<<<< HEAD
 $stmt = $conn->prepare("SELECT * FROM products ORDER BY fldproductid DESC LIMIT 1");
-=======
-
-$stmt = $conn->prepare("SELECT * FROM products ORDER BY fldproductid DESC LIMIT 1");
-
->>>>>>> 26e69de870d2baedd1981e00d0ed48f3a3184a49
 if($stmt->execute()){
   $offerproducts = $stmt->get_result();// This is an array
 }
